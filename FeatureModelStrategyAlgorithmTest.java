539e59aee6057718cfa71b51621bc353bb24cc75
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2014-11-20 01:52:42 -0500

    feature node testing implemented, link testing missing

--- FeatureModelStrategyAlgorithmTest.java
+++ FeatureModelStrategyAlgorithmTest.java
@@ -44,6 +44,7 @@ import ucm.map.StartPoint;
 import ucm.map.Stub;
 import ucm.map.WaitingPlace;
 import urn.URNspec;
+import urncore.Metadata;
 import urncore.UCMmodelElement;
 import urncore.impl.MetadataImpl;
 
@@ -75,11 +76,27 @@ public class FeatureModelStrategyAlgorithmTest extends TestCase {
 
     public URNspec urnspec;
     
+    /** Strategies */
+    private final static int NO_SELECTION = 0;
+    
+    /** Feature Nodes */
     private final static String ROOT = "Root_";
     private final static String PCHILD1 = "PChild1_";
     private final static String PCHILD2 = "PChild2_";
     private final static String CHILD1 = "Child1_";
     private final static String CHILD2 = "Child2_";
+    
+    /** Feature Node Metadata */
+    private final static String NUMEVAL = "_numEval";
+    private final static String QUALEVAL = "_qualEval";
+    private final static String QUALEVAL_SATISFIED = "Satisfied";
+    private final static String QUALEVAL_NONE = "None";
+    private final static String AUTOSELECTED = "_autoSelected";
+    private final static String WARNING = "_userSetEvaluationWarning";
+    private final static String WARNING_MSG = "100 != 0";
+    
+    /** Errors */
+    private final static String UNKNOWN_NODE = "Unknown node";
 
     /*
      * @see TestCase#setUp()
@@ -135,24 +152,92 @@ public class FeatureModelStrategyAlgorithmTest extends TestCase {
         StrategyEvaluationPreferences.setFillElements(true);
         
         StrategyEvaluationPreferences.getPreferenceStore().setValue(StrategyEvaluationPreferences.PREF_AUTOSELECTMANDATORYFEATURES, true);
-        
-//        EvaluationStrategy strategy = (EvaluationStrategy) urnspec.getGrlspec().getStrategies().get(0);
-//		EvaluationStrategyManager.getInstance(editor).setStrategy(strategy);
 	}
     
     @Test
     public void test1() {
-    	
+		final int TABNUMBER = 1;
+		
+		FeatureDiagram featureD = (FeatureDiagram) urnspec.getUrndef().getSpecDiagrams().get(TABNUMBER - 1);
+		
+		EvaluationStrategy strategy = (EvaluationStrategy) urnspec.getGrlspec().getStrategies().get(NO_SELECTION);
+		EvaluationStrategyManager.getInstance(editor).setStrategy(strategy);
+
+		assertEquals(0, featureD.getNodes().size());
+		assertEquals(0, featureD.getConnections().size());
     }
     
     @Test
     public void test2() {
-    	
+    	final int TABNUMBER = 2;
+		
+		EvaluationStrategy strategy = (EvaluationStrategy) urnspec.getGrlspec().getStrategies().get(NO_SELECTION);
+		EvaluationStrategyManager.getInstance(editor).setStrategy(strategy);
+		
+		FeatureDiagram featureD = (FeatureDiagram) urnspec.getUrndef().getSpecDiagrams().get(TABNUMBER - 1);
+		
+		assertEquals(1, featureD.getNodes().size());
+		
+		Iterator elemItr = featureD.getNodes().iterator();
+		
+		while (elemItr.hasNext()) {
+			IntentionalElementRefImpl feature = (IntentionalElementRefImpl) elemItr.next();
+
+			if (hasName(feature, ROOT, TABNUMBER)) {
+				checkNotSelected(feature);
+			} else if (hasName(feature, PCHILD1, TABNUMBER)) {
+				fail(getFeatureName(PCHILD1, TABNUMBER) + " should not exist");
+			} else if (hasName(feature, PCHILD2, TABNUMBER)) {
+				fail(getFeatureName(PCHILD2, TABNUMBER) + " should not exist");
+			} else if (hasName(feature, CHILD1, TABNUMBER)) {
+				fail(getFeatureName(CHILD1, TABNUMBER) + " should not exist");
+			} else if (hasName(feature, CHILD2, TABNUMBER)) {
+				fail(getFeatureName(CHILD2, TABNUMBER) + " should not exist");
+			} else {
+				fail(UNKNOWN_NODE);
+			}
+		}
+		
+		assertEquals(0, featureD.getConnections().size());
     }
     
     @Test
     public void test3() {
-    	
+    	final int TABNUMBER = 3;
+		
+		EvaluationStrategy strategy = (EvaluationStrategy) urnspec.getGrlspec().getStrategies().get(NO_SELECTION);
+		EvaluationStrategyManager.getInstance(editor).setStrategy(strategy);
+		
+		FeatureDiagram featureD = (FeatureDiagram) urnspec.getUrndef().getSpecDiagrams().get(TABNUMBER - 1);
+		
+		/* FEATURE TEST */
+		Iterator elemItr = featureD.getNodes().iterator();
+		
+		while (elemItr.hasNext()) {
+			IntentionalElementRefImpl feature = (IntentionalElementRefImpl) elemItr.next();
+
+			if (hasName(feature, ROOT, TABNUMBER)) {
+				checkNotSelected(feature);
+			} else if (hasName(feature, PCHILD1, TABNUMBER)) {
+				fail(getFeatureName(PCHILD1, TABNUMBER) + " should not exist");
+			} else if (hasName(feature, PCHILD2, TABNUMBER)) {
+				fail(getFeatureName(PCHILD2, TABNUMBER) + " should not exist");
+			} else if (hasName(feature, CHILD1, TABNUMBER)) {
+				fail(getFeatureName(CHILD1, TABNUMBER) + " should not exist");
+			} else if (hasName(feature, CHILD2, TABNUMBER)) {
+				fail(getFeatureName(CHILD2, TABNUMBER) + " should not exist");
+			} else {
+				fail(UNKNOWN_NODE);
+			}
+		}
+		
+		/* LINK TEST */
+	LinkRefImpl rPC1, rPC2, pCC1, pCC2;
+	elemItr = featureD.getConnections().iterator();
+	rPC1 = (LinkRefImpl) elemItr.next();
+	rPC2 = (LinkRefImpl) elemItr.next();
+	pCC1 = (LinkRefImpl) elemItr.next();
+	pCC2 = (LinkRefImpl) elemItr.next();
     }
     
     @Test
@@ -188,7 +273,7 @@ public class FeatureModelStrategyAlgorithmTest extends TestCase {
 		
 		FeatureDiagram featureD = (FeatureDiagram) urnspec.getUrndef().getSpecDiagrams().get(TABNUMBER - 1);
 		
-		EvaluationStrategy strategy = (EvaluationStrategy) urnspec.getGrlspec().getStrategies().get(0);
+		EvaluationStrategy strategy = (EvaluationStrategy) urnspec.getGrlspec().getStrategies().get(NO_SELECTION);
 		EvaluationStrategyManager.getInstance(editor).setStrategy(strategy);
 
 		// Get the feature nodes.
@@ -249,7 +334,76 @@ public class FeatureModelStrategyAlgorithmTest extends TestCase {
 		System.out.println("for break point");
 	}
 	
+    private static void checkAutoSelectedWithWarning(IntentionalElementRefImpl feature) {
+		checkFeatureMetadata(feature, true, "100", true, QUALEVAL_SATISFIED, true, true, WARNING_MSG);
+    }
+    
+    private static void checkAutoSelectedWithoutWarning(IntentionalElementRefImpl feature) {
+		checkFeatureMetadata(feature, true, "100", true, QUALEVAL_SATISFIED, true, false, WARNING_MSG);
+    }
+    
+    private static void checkNotSelected(IntentionalElementRefImpl feature) {
+		checkFeatureMetadata(feature, true, "0", true, QUALEVAL_NONE, false, false, WARNING_MSG);
+    }
+    
+    private static void checkPropagationSelected(IntentionalElementRefImpl feature) {
+		checkFeatureMetadata(feature, true, "100", true, QUALEVAL_SATISFIED, false, false, WARNING_MSG);
+    }
+    
+    private static void checkFeatureMetadata(IntentionalElementRefImpl feature, boolean numEval, String numEvalValue,
+    		boolean qualEval, String qualEvalValue, boolean autoSelection, boolean warning, String warningValue) {
+    	boolean numExists = false;
+		boolean qualExists = false;
+		boolean autoExists = false;
+		boolean warnExists = false;
+		
+		Iterator metaItr = feature.getDef().getMetadata().iterator();
+		
+		while (metaItr.hasNext()) {
+			Metadata meta = (Metadata) metaItr.next();
+			
+			if (isNumEval(meta.getName())) {
+				numExists = true;
+				assertEquals(numEvalValue, meta.getValue());
+			} else if (isQualEval(meta.getName())) {
+				qualExists = true;
+				assertEquals(qualEvalValue, meta.getValue());
+			} else if (isAutoSelected(meta.getName())) {
+				autoExists = true;
+				assertNotNull(meta.getValue());
+			} else if (isWarning(meta.getName())) {
+				warnExists = true;
+				assertEquals(warningValue, meta.getValue());
+			}
+		}
+		
+		assertEquals(numEval, numExists);
+		assertEquals(qualEval, qualExists);
+		assertEquals(autoSelection, autoExists);
+		assertEquals(warning, warnExists);
+    }
+	
+	private static String getFeatureName(String featureName, int diagramTabNumber) {
+		return featureName + diagramTabNumber;
+	}
+	
 	private static boolean hasName(IntentionalElementRefImpl feature, String featureName, int diagramTabNumber) {
-		return feature.getDef().getName().equals(featureName + diagramTabNumber);
+		return feature.getDef().getName().equals(getFeatureName(featureName, diagramTabNumber));
+	}
+	
+	private static boolean isNumEval(String name) {
+		return name.equals(NUMEVAL);
+	}
+	
+	private static boolean isQualEval(String name) {
+		return name.equals(QUALEVAL);
+	}
+	
+	private static boolean isAutoSelected(String name) {
+		return name.equals(AUTOSELECTED);
+	}
+	
+	private static boolean isWarning(String name) {
+		return name.equals(WARNING);
 	}
 }
